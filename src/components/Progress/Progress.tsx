b79272a4066d1e7ecafe5ddd96b4db27cd5341d9
Carrie Forde <carrie@carrieforde.com>
2022-06-15 13:10:57 -0700

    fix: #5 move styles to separate file

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,24 +1,19 @@
-import { LinearProgress, Typography, Box } from "@mui/material";
 import React from "react";
+import {
+  ProgressBar,
+  ProgressContainer,
+  ProgressValue,
+} from "./Progress.styles";
 
 export type ProgressProps = {
   progress: number;
 };
 
 export const Progress: React.FC<ProgressProps> = ({ progress }) => (
-  <Box sx={{ display: "flex", alignItems: "center" }}>
-    <LinearProgress
-      variant="determinate"
-      value={progress}
-      sx={{ flexBasis: "100%" }}
-    />
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      sx={{ flexShrink: 0 }}
-      ml={1}
-    >
+  <ProgressContainer>
+    <ProgressBar variant="determinate" value={progress} />
+    <ProgressValue variant="body2" color="text.secondary">
       {Math.round(progress)}%
-    </Typography>
-  </Box>
+    </ProgressValue>
+  </ProgressContainer>
 );
